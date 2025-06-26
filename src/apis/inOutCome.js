ce22cefcd1eb4733d4d1f9e5c715b4038758dc39
Rainaftbow <17877734735@163.com>
2025-06-26 16:20:15 +0800

    fix: api

--- src/apis/inOutCome.js
+++ src/apis/inOutCome.js
@@ -5,4 +5,4 @@ export const getCategoryList = (type) => service.get('/category/list', {params: 
 export const getBillList = (params) => service.get('/bill/list', {params})
 export const getBillInId = (id) => service.get(`/bill/${id}`)
 export const updateBill = (data) => service.put(`/bill`, data)
-export const deleteBill = (ids) => service.delete(`/bill`, {params: {ids}})
+export const deleteBill = (ids) => service.delete(`/bill`, {params: ids})
